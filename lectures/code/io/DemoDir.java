1c0549b8f81fc8704b27c14384aedc46655634cd
Terence Parr <parrt@cs.usfca.edu>
2014-08-29 15:41:11 -0700

    update example

--- lectures/code/io/DemoDir.java
+++ lectures/code/io/DemoDir.java
@@ -6,7 +6,12 @@ public class DemoDir {
 	public static void main(String[] args)  throws IOException {
 		String fileName = args[0];
 		File f = new File(fileName);
-		String dirListing[] = f.list();
-		System.out.println(Arrays.toString(dirListing));
+		if ( f.exists() && f.isDirectory() ) {
+			String dirListing[] = f.list();
+			System.out.println(Arrays.toString(dirListing));
+		}
+		else {
+			System.err.println("Doesn't exist or isn't a dir");
+		}
 	}
 }
