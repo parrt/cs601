9ca60bdbec1fdffd84a1999f9a7004e4706abe29
Terence Parr <parrt@cs.usfca.edu>
2014-08-29 16:10:56 -0700

    update examples

--- lectures/code/io/ReadStdinText.java
+++ lectures/code/io/ReadStdinText.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ReadText {
+public class ReadStdinText {
 	public static void main(String[] args) throws IOException {
-		String fileName = args[0];
-		FileReader fr = new FileReader(fileName);
-		BufferedReader reader = new BufferedReader(fr);
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(isr);
 		String line = reader.readLine();
 		while (line != null) {
 			// Process line
