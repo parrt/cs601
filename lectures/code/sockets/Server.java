ca0ca17990474b3c0e0319e27d93a6fda634afb2
Terence Parr <parrt@cs.usfca.edu>
2015-02-17 13:33:57 -0800

    tweaks

--- lectures/code/sockets/Server.java
+++ lectures/code/sockets/Server.java
@@ -1,14 +1,12 @@
 import java.net.*;
 import java.io.*;
 
-// http://java.sun.com/j2se/1.3/docs/api/java/net/Socket.html
-// http://java.sun.com/j2se/1.3/docs/api/java/net/ServerSocket.html
-
 public class Server {
 	public static void main(String[] args) throws IOException {
 		int port = Integer.valueOf(args[0]);
 		ServerSocket s = new ServerSocket(port);
 		while ( true ) {
+			System.out.println("waiting for connection...");
 			Socket channel = s.accept();
 			OutputStream out = channel.getOutputStream();
 			InputStream in = channel.getInputStream();
